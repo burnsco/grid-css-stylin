ed0e12457b8d22861dc76e23ca77e2c2897bf217
Corey Burns <coreymburns@gmail.com>
2020-05-21 15:48:37 -0400

    mobile first basic wireframe layout finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+
+const AppGrid = styled.main`
+  grid-gap: 3em;
+  margin: 1em;
+  display: grid;
+`
+const Header = styled.header`
+  height: 60px;
+  border: 3px solid red;
+`
+const AboutMe = styled.div`
+  height: 400px;
+  border: 3px solid blue;
+`
+const MyPic = styled.div`
+  height: 400px;
+  border: 3px solid purple;
+`
+const Contact = styled.div`
+  height: 500px;
+  border: 3px solid brown;
+`
+const ProjectCard = styled.div`
+  height: 400px;
+  border: 3px solid purple;
+`
+const ProjectInfo = styled.div`
+  height: 400px;
+  border: 3px solid blue;
+`
+const MiniApps = styled.div`
+  border: 2px solid green;
+  display: grid;
+  grid-gap: 0.5em;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+`
+const Footer = styled.footer`
+  height: 400px;
+  border: 3px solid orange;
+`
+const SmallApp = styled.div`
+  color: red;
+  border: 3px solid red;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header>Header</Header>
+      <AppGrid>
+        <AboutMe>About Me</AboutMe>
+        <MyPic>My Picture</MyPic>
+        <ProjectCard>Project Card</ProjectCard>
+        <ProjectInfo>Project Info</ProjectInfo>
+        <MiniApps>
+          <SmallApp>
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+          <SmallApp>
+            {' '}
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+          <SmallApp>
+            {' '}
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+          <SmallApp>
+            {' '}
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+          <SmallApp>
+            {' '}
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+          <SmallApp>
+            {' '}
+            <a href="https://placeholder.com">
+              <img src="https://via.placeholder.com/250"></img>
+            </a>
+          </SmallApp>
+        </MiniApps>
+      </AppGrid>
+      <Contact>Contact</Contact>
+      <Footer>Footer</Footer>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import {normalize} from 'styled-normalize'
+import App from './App'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    ${normalize}
+    *,
+    *:before,
+    *:after {
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById('root'),
+)
